62331216affacfa6f2d204c4f53849b09175ebad
Adam Jakubiak <a.jakubiak.srem@gmail.com>
2022-02-21 14:03:35 +0100

    Added header and routing

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col" id="logo">sklepik.pl</div>
+        <router-link to="/main"> <div class="col">Strona Główna</div></router-link>
+        <router-link to="/offer"><div class="col">Oferta</div></router-link>
+        <router-link to="/contact"><div class="col">Kontakt</div></router-link>
+      </div>
+    </div>
+  </header>
+    
+  <main>
+    Witaj na naszej stronie....
+  </main>
+
 </template>
 
 <script>
@@ -8,3 +22,19 @@ export default {
   name: 'App'
 }
 </script>
+
+<style>
+@import 'C:/Users/Adam/Documents/zadania/sklepik/app/node_modules/bootstrap/dist/css/bootstrap.css';
+header{
+  float:left;
+  width:100%;
+  height:100px;
+  background-color: #812b81;
+  color:white;
+  font-size: x-large;
+}
+#logo{
+ float: left;
+ margin-right: 600px;
+}
+</style>
